6260bfd0fa3fc7c5b2355792da34abc1f59ff8e8
Yushenko Dima <damios.1812@gmail.com>
2025-05-06 09:57:53 +0200

    add start function for form

---
+++ src/js/modules/forms.js
@@ -0,0 +1,89 @@
+import checkNumInputs from "./CheckNumInputs";
+
+const forms = (state) => {
+    const form = document.querySelectorAll('form');
+    const input = document.querySelectorAll('input');
+
+    // checkNumInputs('input[name="user_phone"]');
+
+    const message = {
+        loading: 'Загрузка...',
+        success: 'Спасибо! Скоро мы с вами свяжемся',
+        failure: 'Что-то пошло не так',
+        spinner: 'assets/img/spinner.gif',
+        ok: 'assets/img/ok.png',
+        fail: 'assets/img/fail.png'
+    };
+
+    const path = {
+        designer: 'assets/server.php',
+        question: 'assets/question.php'
+    }
+
+    const postData = async (url, data) => {
+
+        let res = await fetch(url, {
+            method: "POST",
+            body: data
+        });
+
+        return await res.text();
+    }
+
+    const clearInputs = () => {
+        input.forEach(item => {
+            item.value = '';
+        })
+    }
+
+    form.forEach(item => {
+        item.addEventListener('submit', (e) => {
+            e.preventDefault();
+
+            let statusMessage = document.createElement('div');
+            statusMessage.classList.add('status');
+            item.parentNode.appendChild(statusMessage);
+
+            let textMessage = document.createElement('div');
+            textMessage.textContent = message.loading;
+            statusMessage.appendChild(textMessage);
+
+            item.classList.add('animated', 'fadeOut');
+            setTimeout(() => {
+                item.style.display = 'none';
+            }, 400)
+
+            let statusImg = document.createElement('img');
+            statusImg.setAttribute('src', message.spinner);
+            statusImg.classList.add('animated', 'fadeInUp');
+            statusMessage.appendChild(statusImg);
+
+            const formData = new FormData(item);
+
+            if (item.getAttribute('data-calc') === "end") {
+                for (let key in state) {
+                    formData.append(key, state[key]);
+                }
+            }
+
+            state = {};
+
+            postData('assets/server.php', formData)
+                .then(res => {
+                    console.log(res);
+                    statusMessage.textContent = message.success;
+                })
+                .catch(() => {
+                    statusMessage.textContent = message.failure;
+                })
+                .finally(() => {
+                    clearInputs();
+                    setTimeout(() => {
+                        statusMessage.remove();
+                    }, 5000);
+                })
+        })
+    })
+}
+
+export default forms;
