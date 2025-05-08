7f8c6b3f29b1123613119f6520a547a8012c1aea
Yushenko Dima <damios.1812@gmail.com>
2025-05-08 09:31:44 +0200

    add function showItem for button

--- src/js/main.js
+++ src/js/main.js
@@ -3,6 +3,7 @@ import sliders from "./modules/sliders";
 import forms from "./modules/forms";
 import mask from "./modules/mask";
 import checkTextInputs from "./modules/checkTextInputs";
+import showMoreStyles from "./modules/showMoreStyles";
 
 document.addEventListener('DOMContentLoaded', () => {
     "use strict";
@@ -14,4 +15,5 @@ document.addEventListener('DOMContentLoaded', () => {
     mask('[name="phone"]');
     checkTextInputs('[name="name"]');
     checkTextInputs('[name="message"]');
+    showMoreStyles('.button-styles', '.styles-2')
 })
